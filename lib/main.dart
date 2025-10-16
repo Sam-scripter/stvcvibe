505f663f02cd1f9a4d453fa7bf8a7eb90929d1fd
Samuel Shadiva <leamasflature@gmail.com>
2025-10-16 04:45:29 +0300

    prototype update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stvcvibe/prototype/mvp_illustration.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StvcVibe());
 }
 
